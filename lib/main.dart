838363e4e45ec311862dcd127da3ca15377a5c4a
nuel232 <nwankwoala3@gmail.com>
2025-08-20 23:54:56 +0100

    feat: integrate provider for theme management and enhance settings page
    - Add provider dependency for state management
    - Implement ThemeProvider to manage light/dark mode
    - Update main.dart to wrap MyApp with ChangeNotifierProvider
    - Enhance SettingsPage with a toggle switch for dark mode functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:chat_app/services/auth/auth_gate.dart';
 import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/themes/light_mode.dart';
+import 'package:chat_app/themes/theme_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   //This ensures Flutter is ready before we do anything
   WidgetsFlutterBinding.ensureInitialized();
   // Connect to Firebase - this is like plugging into the internet
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -20,7 +27,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: AuthGate(),
-      theme: lightMode,
+      theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
 }
--- lib/pages/Settings_page.dart
+++ lib/pages/Settings_page.dart
@@ -1,4 +1,7 @@
+import 'package:chat_app/themes/theme_provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -12,6 +15,34 @@ class SettingsPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.transparent,
         foregroundColor: Colors.grey,
+      ),
+
+      body: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        padding: EdgeInsets.all(20),
+        margin: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            //dark mode
+            Text('Dark mode'),
+
+            // switch toggle
+            CupertinoSwitch(
+              value: Provider.of<ThemeProvider>(
+                context,
+                listen: false,
+              ).isDarkMode,
+              onChanged: (value) => Provider.of<ThemeProvider>(
+                context,
+                listen: false,
+              ).toggleTheme(),
+            ),
+          ],
+        ),
       ),
     );
   }
